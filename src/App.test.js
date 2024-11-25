49c742a3f63a0a8a97e276bbc8bbb4a1ab0ca0ea
Alexander Wong <lexwon@users.noreply.github.com>
2024-11-25 14:29:59 -0500

    Created basic tests
    package*.json - updated testing library and added msw
    App.test.js - added basic render test
    setupTests.js / src/testHelpers/*- added msw setup (not enabled yet)

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,30 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Demo Chatbot tests", () => {
+  test("renders the chatbot page", async () => {
+    render(<App />);
+
+    expect(
+      await screen.findByRole("heading", { name: "Chatbot Demo" })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("heading", {
+        name: "This is a demo chatbot built using create-react-app and tailwindcss",
+      })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText("Enter your message into the chatbox to being chatting")
+    );
+    expect(
+      screen.getByRole("textbox", {
+        name: "chatbox input",
+      })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("button", {
+        name: "send",
+      })
+    ).toBeInTheDocument();
+  });
 });
