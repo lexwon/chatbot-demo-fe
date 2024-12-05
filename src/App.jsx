6a8d8e7f65005f89eefb848ce45bcf0b4e8313f6
Alexander Wong <lexwon@users.noreply.github.com>
2024-12-05 16:51:58 -0500

    removed loading state and added search
    App.jsx - removed the loading state and added search functionality. Also separated when prompts and responses are added to state
    output.css - build output

--- src/App.jsx
+++ src/App.jsx
@@ -30,28 +30,29 @@ const MessageTypes = {
 function Chatbot() {
   const [messages, setMessages] = useState([]);
   const [prompt, setPrompt] = useState("");
-  const { loading, error, postData } = useFetch();
+  const { error, postData } = useFetch();
+  const [searchTerm, setSearchTerm] = useState("");
+
   const sendPrompt = async () => {
     if (prompt) {
-      await postData({
+      setPrompt("");
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        { text: prompt, type: MessageTypes.prompt },
+      ]);
+
+      const data = await postData({
         url: "http://localhost:8000/api/chat",
         body: JSON.stringify({ message: prompt }),
-      }).then((data) => {
-        if (data) {
-          setPrompt("");
-          setMessages([
-            ...messages,
-            { text: prompt, type: MessageTypes.prompt },
-            { text: data.response, type: MessageTypes.response },
-          ]);
-        }
       });
+      if (data) {        
+        setMessages((prevMessages) => [
+          ...prevMessages,
+          { text: data.response, type: MessageTypes.response },
+        ]);
+      }
     }
   };
-
-  if (loading) {
-    return <h3>Loading ....</h3>;
-  }
 
   if (error) {
     return <h3>There was an error communicating to the Chatbot</h3>;
@@ -59,17 +60,33 @@ function Chatbot() {
 
   return (
     <Flex className="basis-1/3 flex-col">
+      <Flex className="py-3 flex-row justify-center">
+        <input
+          aria-label="search chat"
+          className="border-solid border-black border rounded px-3"
+          value={searchTerm}
+          onChange={(value) => {
+            setSearchTerm(value.target.value);
+          }}
+        />
+      </Flex>
       <div className="bg-slate-100 border-solid border-black rounded">
-        {messages.map((message, index) => (
-          <div key={index}>
-            {message.type === MessageTypes.prompt && (
-              <Prompt text={message.text} />
-            )}
-            {message.type === MessageTypes.response && (
-              <Response text={message.text} />
-            )}
-          </div>
-        ))}
+        {messages
+          .filter((message) =>
+            searchTerm.trim().length > 0
+              ? message.text.toLowerCase().includes(searchTerm.toLowerCase())
+              : message
+          )
+          .map((message, index) => (
+            <div key={index}>
+              {message.type === MessageTypes.prompt && (
+                <Prompt text={message.text} />
+              )}
+              {message.type === MessageTypes.response && (
+                <Response text={message.text} />
+              )}
+            </div>
+          ))}
       </div>
       <Flex className="py-3 flex-row justify-center">
         <input
